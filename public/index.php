9087bb726da2f63db636b442947a6df18a1bf1fc
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-04-16 09:03:08 +0200

    rooting are working

--- public/index.php
+++ public/index.php
@@ -6,19 +6,44 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+/**
+ * Instantiate App
+ *
+ * In order for the factory to work you need to ensure you have installed
+ * a supported PSR-7 implementation of your choice e.g.: Slim PSR-7 and a supported
+ * ServerRequest creator (included with Slim PSR-7)
+ */
 $app = AppFactory::create();
 
-//$app->setBasePath(rtrim(dirname($_SERVER["REQUEST_URI"]), '/'));
-$app->setBasePath('Projectpool3/autocompletion/public/');
+$app->setBasePath('/Projectpool3/autocompletion/public');
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+/**
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+ 
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write($_SERVER["REQUEST_URI"]);
+    $response->getBody()->write("Hello tout court");
     return $response;
 });
 
-$app->get('/test', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world test!");
-    return $response;
-});
-
+// Run app
 $app->run();
